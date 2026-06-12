e584e677bd978de6be312f24c5145ab95b2525e3
chrisantuseze <ezechris30@gmail.com>
2026-06-12 14:54:33 -0500

    update unzip script to process filtered task archive

--- unzip.py
+++ unzip.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 import zipfile
 
-with zipfile.ZipFile("task_ABC_D.zip", 'r') as zip_ref:
-    zip_ref.extractall("task_ABC_D")
+with zipfile.ZipFile("task_ABCD_D_filtered.zip", 'r') as zip_ref:
+    zip_ref.extractall("task_ABCD_D_filtered")
